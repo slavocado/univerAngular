4f24749e15afc3af5f0d9656091d67a08d51358c
Viacheslav <little.cado1@gmail.com>
2021-01-19 18:47:35 +0300

    Add calculate age function

--- project16/src/app/ui/table-workers/table-workers.component.ts
+++ project16/src/app/ui/table-workers/table-workers.component.ts
@@ -11,11 +11,17 @@ export class TableWorkersComponent implements OnInit {
   @Input() workers: MyWorker[] = [];
   @Input() title: string = '';
 
-  displayedColumns: string[] = ['id', 'name', 'surname', 'age'];
+  displayedColumns: string[] = ['id', 'name', 'surname', 'age', 'actions'];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  calculateAge(birthday){
+      const birthdayDate = new Date(birthday);
+      const timeDiff = Math.abs(Date.now() - birthdayDate.getTime());
+      return Math.floor((timeDiff / (1000 * 3600 * 24))/365);
   }
 
 }
