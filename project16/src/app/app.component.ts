b55e73685590d13cfa53006b30a1d8e01faf5e11
KhDmitrii <kholodov.d@gmail.com>
2021-01-22 11:13:14 +0300

    fixes

--- project16/src/app/app.component.ts
+++ project16/src/app/app.component.ts
@@ -1,72 +1,102 @@
-import {Component, OnInit} from '@angular/core';
-import {MyWorker, MyWorkerType} from "./shared/worker.model";
-import {HttpWorkerService} from "./shared/services/http-worker.service";
-
+import { Component, OnInit } from '@angular/core';
+import { MyWorker, MyWorkerType } from './shared/worker.model';
+import { HttpWorkerService } from './shared/services/http-worker.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'project16';
   workers: MyWorker[];
   myWorkerType = MyWorkerType;
 
-  constructor(private httpWorkerService: HttpWorkerService) { }
+  dataSources: {
+    type: number | string;
+    source: MatTableDataSource<any>;
+  }[] = [];
+
+  constructor(private httpWorkerService: HttpWorkerService) {}
 
   ngOnInit() {
     this.getData();
   }
 
-  async getData(){
+  async getData() {
     try {
       this.workers = await this.httpWorkerService.getWorkers();
       console.log(this.workers)
-    } catch (e){
-      console.error(e)
+    } catch (e) {
+      console.error(e);
     }
+    /**
+     * После получения даных про работников из АПИ формируем массив dataSource для таблиц. Это сложно сделать
+     * в дочерних компонентах т.к. данные из АПИ приходят условно в любое время а не при инициализации,
+     * а на их основе нужно сформировать dataSource
+     */
+    let values = Object.values(this.myWorkerType).filter(
+      (val) => typeof val === 'number'
+    );
+    this.dataSources = [];
+    for (let value in values) {
+      this.dataSources.push({
+        type: +value,
+        source: new MatTableDataSource(this.getByType(+value)),
+      });
+    }
+  }
+
+  /**
+   * Функция выдает  выдавала нужный dataSource и отправляет его в дочерний компонент
+   * (аналогично предыдущему решению) толькол dataSource
+   * @param type
+   */
+  getDataSourceByType(type: number) {
+    return this.dataSources.find((ds) => ds.type == type)?.source;
   }
 
   getByType(type: number) {
-    if(this.workers){
-      return this.workers.filter(worker => worker.type === type);
+    if (this.workers) {
+      return this.workers.filter((worker) => worker.type === type);
     }
   }
 
-  async onDeleteWorker(id: number){
+  async onDeleteWorker(id: number) {
     try {
-      await this.httpWorkerService.deleteWorker(id)
+      await this.httpWorkerService.deleteWorker(id);
     } catch (e) {
-      console.error(e)
+      console.error(e);
     } finally {
-      await this.getData()
+      await this.getData();
     }
   }
 
-  async onAddWorker(worker: MyWorker){
-    const id = this.workers.length > 0
-      ? this.workers[this.workers.length - 1].id + 1
-      : 0
+  async onAddWorker(worker: MyWorker) {
+    const id =
+      this.workers.length > 0
+        ? this.workers[this.workers.length - 1].id + 1
+        : 0;
     worker.id = id;
     // this.workers.push(worker);
 
     try {
-      await this.httpWorkerService.postWorker(worker)
+      await this.httpWorkerService.postWorker(worker);
     } catch (e) {
       console.error(e);
     } finally {
-      await this.getData()
+      await this.getData();
     }
   }
 
-  async onEditWorker(editingWorker){
+  async onEditWorker(editingWorker) {
     try {
-      await this.httpWorkerService.putWorker(editingWorker)
-    } catch (e){
-      console.error(e)
+      await this.httpWorkerService.putWorker(editingWorker);
+    } catch (e) {
+      console.error(e);
     } finally {
-      await this.getData()
+      await this.getData();
     }
   }
 }
--- project16/src/app/ui/table-workers/table-workers.component.ts
+++ project16/src/app/ui/table-workers/table-workers.component.ts
@@ -1,57 +1,79 @@
-import {Component, Input, OnInit, Output, EventEmitter, AfterViewInit, ViewChild} from '@angular/core';
-import {MyWorker} from '../../shared/worker.model';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import {
+  Component,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+  AfterViewInit,
+  ViewChild,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { MyWorker } from '../../shared/worker.model';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-table-workers',
   templateUrl: './table-workers.component.html',
-  styleUrls: ['./table-workers.component.css']
+  styleUrls: ['./table-workers.component.css'],
 })
-export class TableWorkersComponent implements OnInit, AfterViewInit {
-
-  @Input() workers: MyWorker[] = [];
+export class TableWorkersComponent implements OnInit, AfterViewInit, OnChanges {
+  // @Input() workers: MyWorker[] = []; - теперь не нужно
   @Input() title: string = '';
 
-  @Output() editWorker =
-    new EventEmitter<object>();
-  @Output() deleteWorker =
-    new EventEmitter<number>();
+  @Output() editWorker = new EventEmitter<object>();
+  @Output() deleteWorker = new EventEmitter<number>();
 
-  dataSource: MatTableDataSource<any>;
+  @Input() dataSource: MatTableDataSource<any>; // а это делаем входным параметром
 
   @ViewChild(MatSort) sort: MatSort;
 
   ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
+    // this.dataSource.sort = this.sort;
   }
 
   displayedColumns: string[] = ['id', 'name', 'surname', 'age', 'actions'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-    if (this.workers.length > 0){
-      this.dataSource = new MatTableDataSource(this.workers)
-      console.log('================')
-      console.log(this.dataSource)
-      console.log(this.workers)
-      console.log('================')
+  /**
+   * Повесить привязку сортировки на ngAfterViewInit нельзя т.к. dataSource будет получен в неопределенное время
+   * по этому используем ngOnChanges https://angular.io/guide/lifecycle-hooks
+   * и смотрим ситуацию, когда значенрие входного параметра dataSource изменилось с undefined на не undefined
+   * т.е. пришло от родителя
+   * @param changes
+   */
+  ngOnChanges(changes: SimpleChanges) {
+    if (
+      changes.dataSource.previousValue === undefined &&
+      changes.dataSource.currentValue !== undefined
+    ) {
+      this.dataSource.sort = this.sort;
     }
   }
 
-  calculateAge(birthday){
-      const birthdayDate = new Date(birthday);
-      const timeDiff = Math.abs(Date.now() - birthdayDate.getTime());
-      return Math.floor((timeDiff / (1000 * 3600 * 24))/365);
+  ngOnInit(): void {
+    // if (this.workers.length > 0) {
+    //   this.dataSource = new MatTableDataSource(this.workers);
+    //   console.log('================');
+    //   console.log(this.dataSource);
+    //   console.log(this.workers);
+    //   console.log('================');
+    // }
   }
 
-  onEditWorker(worker){
-    this.editWorker.emit(worker)
+  calculateAge(birthday) {
+    const birthdayDate = new Date(birthday);
+    const timeDiff = Math.abs(Date.now() - birthdayDate.getTime());
+    return Math.floor(timeDiff / (1000 * 3600 * 24) / 365);
   }
 
-  onDeleteWorker(id: number){
-    this.deleteWorker.emit(id)
+  onEditWorker(worker) {
+    this.editWorker.emit(worker);
   }
 
+  onDeleteWorker(id: number) {
+    this.deleteWorker.emit(id);
+  }
 }
